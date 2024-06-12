ba060802532f852f450aa56f021f6ac63ce25cc3
Marcos Santos <marcossantosmsms604@gmail.com>
2024-06-11 23:14:36 -0300

    Projeto criado!

---
+++ src/main/java/com/marcos/livraria_sistemas/LivrariaSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.livraria_sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaSistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/livraria_sistemas/LivrariaSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.livraria_sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
